98c7b131aaf1665ae3c92a28a12b1d5cf2f814b7
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-07-11 00:05:36 +0530

    classNames styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 const Card = ({ title }) => {
   return (
-    <div>
+    <div className="card">
       <h2>{title}</h2>
     </div>
   );
@@ -11,17 +11,18 @@ const Card = ({ title }) => {
 const App = () => {
   return (
     <>
-      <h2>Arrow Function</h2>
-      <br />
-      <Card title="The loin King" />
-      <Card title="Dhurandar" />
-      <Card title="Avatar" />
-      <Card title="The hero hanuman" />
-      <Card title="Star Wars" />
+      <div className="card-container">
+        <br />
+        <Card title="The loin King" />
+        <Card title="Dhurandar" />
+        <Card title="Avatar" />
+        <Card title="The hero hanuman" />
+        <Card title="Star Wars" />
+      </div>
     </>
   );
 };
 export default App;
 // ! https://www.youtube.com/watch?v=dCLhUialKPQ
 
-// ? 22:20
+// ? 25:25
