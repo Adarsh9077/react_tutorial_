a51521e2fdde37b66d9dfb1a4f0bec39ef86900a
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-07-13 23:59:49 +0530

    use efect

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { useEffect } from "react";
 import "./App.css";
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useEffect(false);
   return (
     <div className="card">
       <h2>{title}</h2>
+      <button onclick={() => setHasLiked(true)}></button>
     </div>
   );
 };
 
 const App = () => {
+  const [hasLiked, setHasLiked] = useEffect(false);
   return (
     <>
       <div className="card-container">
