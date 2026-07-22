be74b1b67f235d2e89485bf1101f73eb9ee6c134
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-07-22 23:58:59 +0530

    tailwind attached

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,57 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+
 import "./App.css";
 
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title}</h2>
-      <button
-        onClick={() => {
-          // alert(title);
-          setHasLiked(!hasLiked);
-        }}
-      >
-        {hasLiked ? "Liked " : "Like "}
-      </button>
-    </div>
-  );
-};
+// const Card = ({ title }) => {
+//   const [hasLiked, setHasLiked] = useState(false);
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     console.log(`${title} has been liked: ${hasLiked}`);
+//   }, [hasLiked]);
+//   return (
+//     <div className="card" onClick={() => setCount(count + 1)}>
+//       <h2>{title}</h2>
+//       <button
+//         onClick={() => {
+//           // alert(title);
+//           setHasLiked(!hasLiked);
+//         }}
+//       >
+//         {hasLiked ? "Liked " : "Like "}
+//       </button>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   // const [hasLiked, setHasLiked] = useEffect(false);
+//   return (
+//     <>
+//       <div className="card-container">
+//         <h1>hello</h1>
+//         <br />
+//         <Card title="The loin King" />
+//         <Card title="Dhurandar" />
+//         <Card title="Avatar" />
+//         <Card title="The hero hanuman" />
+//         <Card title="Star Wars" />
+//       </div>
+//     </>
+//   );
+// };
 
 const App = () => {
-  // const [hasLiked, setHasLiked] = useEffect(false);
   return (
     <>
-      <div className="card-container">
-        <h1>hello</h1>
-        <br />
-        <Card title="The loin King" />
-        <Card title="Dhurandar" />
-        <Card title="Avatar" />
-        <Card title="The hero hanuman" />
-        <Card title="Star Wars" />
+      <div className="container">
+        <h1 className="text-3xl font-bold underline text-red-500">
+          hello world
+        </h1>
       </div>
     </>
   );
 };
+
 export default App;
 // ! https://www.youtube.com/watch?v=dCLhUialKPQ
 
